598d26aef4203f6ddf7812b483536f0501ad0aed
indra <ispandey81@gmail.com>
2022-02-12 14:39:41 +0800

    Added unit tests

--- src/main/java/PublicHolidayParser.java
+++ src/main/java/PublicHolidayParser.java
@@ -8,32 +8,41 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class PublicHolidayParser {
+    public static final String PUBLIC_HOLIDAYS_URL = "https://www.commerce.wa.gov.au/labour-relations/public-holidays-western-australia";
 
     public static void main(String[] args) {
-        displayParsedHolidays();
+        connectAndReturnResult();
     }
 
-    public static void displayParsedHolidays() {
-        String publicHolidaysUrl = "https://www.commerce.wa.gov.au/labour-relations/public-holidays-western-australia";
+    public static void connectAndReturnResult() {
         try {
-            Document doc = Jsoup.connect(publicHolidaysUrl).get();
-            Elements divContainingData = doc.select(".field-name-body");
-            Elements yearsOfPublicHolidays = divContainingData.select("th[scope$=col]");
-            Elements namesOfPublicHolidays = divContainingData.select("th[scope$=row] strong");
-            Elements datesOfHolidays = divContainingData.select("td");
-            String years = yearsOfPublicHolidays.stream().map(e -> e.text()).collect(Collectors.joining("->"));
-            List<String> holidays = namesOfPublicHolidays.stream().map(e -> years+" | "+e.text()).collect(Collectors.toList());
-            List<String> dates = datesOfHolidays.stream().map(e -> e.text()).collect(Collectors.toList());
-            int partitionSize = 3;
-            List<List<String>> partitions = new ArrayList<>();
-            for (int i=0; i<dates.size(); i += partitionSize) {
-                partitions.add(dates.subList(i, Math.min(i + partitionSize, dates.size())));
-            }
-            for (int i = 0; i < holidays.size(); i++) {
-                System.out.println(holidays.get(i) + " = " + partitions.get(i));
+            Document doc = Jsoup.connect(PUBLIC_HOLIDAYS_URL).get();
+            List<String> result = parsedHolidays(doc);
+            for (String yearsAndHolidays: result) {
+                System.out.println(yearsAndHolidays);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<String> parsedHolidays(Document document) {
+        Elements divContainingData = document.select(".field-name-body");
+        Elements yearsOfPublicHolidays = divContainingData.select("th[scope$=col]");
+        Elements namesOfPublicHolidays = divContainingData.select("th[scope$=row] strong");
+        Elements datesOfHolidays = divContainingData.select("td");
+        String years = yearsOfPublicHolidays.stream().map(e -> e.text()).collect(Collectors.joining("->"));
+        List<String> holidays = namesOfPublicHolidays.stream().map(e -> years+" | "+e.text()).collect(Collectors.toList());
+        List<String> dates = datesOfHolidays.stream().map(e -> e.text()).collect(Collectors.toList());
+        int partitionSize = 3;
+        List<List<String>> partitions = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        for (int i=0; i<dates.size(); i += partitionSize) {
+            partitions.add(dates.subList(i, Math.min(i + partitionSize, dates.size())));
+        }
+        for (int i = 0; i < holidays.size(); i++) {
+            result.add(holidays.get(i) + " = " + partitions.get(i));
+        }
+        return result;
     }
 }
--- src/test/java/PublichHolidayParserTest.java
+++ src/test/java/PublichHolidayParserTest.java
@@ -1,11 +1,23 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PublichHolidayParserTest {
 
     @Test
     public void testDisplayParsedInfo() {
-//        assert
+        try {
+            Document doc = Jsoup.connect(PublicHolidayParser.PUBLIC_HOLIDAYS_URL).get();
+            List<String> result = PublicHolidayParser.parsedHolidays(doc);
+            assertNotNull(result);
+            assertEquals(11, result.size(), "Expected array size is equal to the actual array size");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
